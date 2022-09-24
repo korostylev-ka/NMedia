4ffafc8da5737260ca9509aab41c58a36e9b7d35
KKorostylev <k.korostylev@gmail.com>
2022-09-25 01:38:22 +0500

    CRUD

--- app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
@@ -1,10 +1,19 @@
 package ru.netology.nmedia.activity
 
+import android.content.Context
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.Group
+import ru.netology.nmedia.R
+import ru.netology.nmedia.adapter.OnInteractionListener
 import ru.netology.nmedia.adapter.PostAdapter
 import ru.netology.nmedia.databinding.ActivityMainBinding
+import ru.netology.nmedia.dto.Post
 import ru.netology.nmedia.viewmodel.PostViewModel
 
 class MainActivity : AppCompatActivity() {
@@ -12,23 +21,85 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
         val viewModel: PostViewModel by viewModels()
-        //создаем адаптер
-        val adapter = PostAdapter(
-            onLikeListener = {
-                viewModel.likeById(it.id)
-            },
-            onShareListener = {
-                viewModel.shareById(it.id)
+        //группа, содержащая текущеее значение контента(текст + кнопка очистки)
+        val group = findViewById<Group>(R.id.group_old)
+        //строка, содержащая текущеее значение контента
+        val textOld = findViewById<EditText>(R.id.content_old)
+        //по нажатию на кнопку send
+        binding.buttonSend.setOnClickListener {
+            with (binding.content) {
+                if (text.isNullOrBlank()) {
+                    Toast.makeText(
+                        this@MainActivity,
+                        context.getString(R.string.error_empty_content),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    return@setOnClickListener
+                }
+                viewModel.changeContent(text.toString())
+                viewModel.save()
+                //очищаем binding content
+                setText("")
+                clearFocus()
+                AndroidUtils.hideKeyboard(this)
             }
-        )
+
+        }
+        //нажатие на кнопку отмены редактирования
+        binding.buttonCancel.setOnClickListener {
+            //делаем группу невидимой
+            group.visibility = View.INVISIBLE
+            viewModel.save()
+            binding.content.setText("")
+            binding.content.clearFocus()
+        }
+
+        //создаем адаптер
+        val adapter = PostAdapter(object : OnInteractionListener {
+            override fun edit(post: Post) {
+                textOld.setText(post.content)
+                //делаем видимую группу
+                group.visibility = View.VISIBLE
+                viewModel.edit(post)
+            }
+
+            override fun remove(post: Post) {
+                viewModel.removeById(post.id)
+            }
+
+            override fun like(post: Post) {
+                viewModel.likeById(post.id)
+            }
+
+            override fun share(post: Post) {
+                viewModel.shareById(post.id)
+            }
+        })
         binding.lists.adapter = adapter
         viewModel.data.observe(this) {posts ->
             adapter.submitList(posts)
         }
+        //нужно сфокусироваться на поле ввода
+        viewModel.edited.observe(this) {
+            if (it.id == 0L) {
+                return@observe
+            }
+            with(binding.content) {
+                setText(it.content)
+                requestFocus()
+            }
+
+        }
     }
 
+}
 
-
-
+object AndroidUtils {
+    fun hideKeyboard(view: View) {
+        val imm = view.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken,0)
+    }
 }
--- app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
+++ app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
@@ -1,12 +1,62 @@
 package ru.netology.nmedia.viewmodel
 
+import androidx.constraintlayout.widget.Group
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import ru.netology.nmedia.R
+import ru.netology.nmedia.dto.Post
 import ru.netology.nmedia.repository.PostRepository
 import ru.netology.nmedia.repository.PostRepositoryInMemoryImpl
+
+private val empty = Post(
+    0L,
+    "",
+    "",
+    "",
+    0,
+    0,
+    0,
+    false
+
+)
+
 
 class PostViewModel: ViewModel() {
     private val repository: PostRepository = PostRepositoryInMemoryImpl()
     val data = repository.getAll()
+    val edited = MutableLiveData(empty)
+    //"заглушка" с пустым постом
+
     fun likeById(id: Long) = repository.likeById(id)
     fun shareById(id: Long) = repository.shareById(id)
+    fun removeById(postId: Long) = repository.removeById(postId)
+
+    //функция сохранения контента(после изменения данных мы отправим в репозиторий измененный пост и сбросим в дефолтное значение
+    fun save() {
+        edited.value?.let {
+            repository.save(it)
+        }
+        edited.value = empty
+    }
+
+    //функция изменения контента
+    fun changeContent(content: String) {
+        //если контент совпал, выходим
+        if (content == edited.value?.content) {
+            return
+        }
+        edited.value = edited.value?.copy(content = content)
+    }
+
+    //функция редактирования
+    fun edit(post: Post){
+        edited.value = post
+
+
+    }
+
+
+
 }
+
+
